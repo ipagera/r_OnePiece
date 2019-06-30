c787d2cbcde238734d43de76ef0c6ba803f9dd1d
Ivan Gerov <igerov11@gmail.com>
2019-06-30 23:40:56 +0200

    Added subreddit attributes in Praw package, tested a bitof its functionalities, added packages to requirements.txt

--- testing_stage.py
+++ testing_stage.py
@@ -1,6 +1,24 @@
+import praw
 
-def function():
-    pass
+reddit = praw.Reddit(client_id = 'xmZYPFS6XU-oyA',
+                    client_secret = 'SlTDyGWqQK1OrdYQvb1ryWJxGyo',
+                    username = 'ipagera',
+                    password = 'Ignigeralt123',
+                    user_agent = 'one_piece_project')
 
-def rOnePiece():
-    pass
+subreddit = reddit.subreddit('onepiece')
+
+hot_python = subreddit.hot(limit=1)
+
+search = subreddit.search("One Piece Chapter 947 Spoilers",sort="relevance",limit=5,time_filter='month')
+
+
+
+# for i in search:
+#     # print(dir(i))
+#     print('Title: ', i.title)
+#     print('Subreddit: ', i.subreddit)
+
+subred_attr = [dir(i) for i in search]
+
+print(subred_attr)
