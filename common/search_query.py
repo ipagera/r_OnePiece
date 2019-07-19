9b665dbe1091bf02d6c0c8ca656fa0940a129646
Ivan Gerov <igerov11@gmail.com>
2019-07-19 20:42:41 +0200

    Developed search_query module functionality and added query_json.db

--- common/search_query.py
+++ common/search_query.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from db_operators import JsonDb
+
 
 json_db = JsonDb("common/posts_db.json")
 
@@ -8,7 +10,7 @@ for key, value in json_db.data.items():
     posts_from_db[key] = json_db.data[key]["created_on"]
 
 
-print(posts_from_db)
+# print(posts_from_db)
 
 
 # TODO
@@ -17,3 +19,32 @@ print(posts_from_db)
 1. New Database for search queries
 2. 
 """
+
+class Query:
+
+    def __init__(self, query_text):
+        self.query_text = query_text
+        self.creation_date = (datetime.now()).timestamp()
+
+class QueryDb(JsonDb):
+    """ Class that initializes search query database. It
+    helps create automatic queries and it inherits from
+    db_operators.JsonDb """
+
+    def __init__(self, filename):
+        super().__init__(filename)
+        
+
+
+    def add_entry(self, data):
+        pass
+
+
+
+
+
+test_query1 = Query('one piece chapter 948 spoilers')
+
+
+query_db = QueryDb('common/query_db.json')
+
